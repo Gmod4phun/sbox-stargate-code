043c02f2229460e1873235eecb97fb0a4933a5f0
Patrik Šimanský <Gmod4phun@gmail.com>
2023-12-30 15:33:40 +0100

    Add a simple crosshair

---
+++ code/Components/Player/Crosshair.cs
@@ -0,0 +1,18 @@
+public class Crosshair : Component
+{
+    [Property]
+    public ModelRenderer CrosshairModel { get; set; }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+
+        if ( CrosshairModel.IsValid() && CrosshairModel.SceneObject is SceneObject so )
+        {
+            CrosshairModel.RenderType = ModelRenderer.ShadowRenderType.Off;
+
+            so.Batchable = false;
+            so.RenderLayer = SceneRenderLayer.OverlayWithoutDepth;
+        }
+    }
+}
