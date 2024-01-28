4dec823fd9c873607e73f5d8f78ef285f4a7ac0e
Patrik Šimanský <Gmod4phun@gmail.com>
2024-01-28 11:56:24 +0100

    Add spin component

---
+++ code/Components/SpinComponent.cs
@@ -0,0 +1,11 @@
+public class SpinComponent : Component
+{
+    [Property] public Angles SpinAngles { get; set; }
+
+    protected override void OnUpdate()
+    {
+        if ( IsProxy ) return;
+
+        Transform.LocalRotation *= (SpinAngles * Time.Delta).ToRotation();
+    }
+}
