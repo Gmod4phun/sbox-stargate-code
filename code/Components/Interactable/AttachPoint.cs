efbd8875cf78397e2b066df647aaeef519b51638
Patrik Šimanský <Gmod4phun@gmail.com>
2024-07-20 16:45:55 +0200

    AttachPoint thresholds for being close enough

--- code/Components/Interactable/AttachPoint.cs
+++ code/Components/Interactable/AttachPoint.cs
@@ -15,6 +15,21 @@ public class AttachPoint : Component, Component.ExecuteInEditor
 	[Property]
 	public bool DebugGuideModel { get; set; }
 
+	[Property]
+	public float AngThreshold = 11.25f;
+
+	[Property]
+	public float PosThreshold = 2f;
+
+	[Property]
+	public bool IgnorePitch { get; set; }
+
+	[Property]
+	public bool IgnoreYaw { get; set; }
+
+	[Property]
+	public bool IgnoreRoll { get; set; }
+
 	public bool IsAttached => CurrentAttachable.IsValid();
 
 	public TimeSince TimeSinceLastDetach = 0;
@@ -57,7 +72,7 @@ public class AttachPoint : Component, Component.ExecuteInEditor
 		if (DefaultPrefab != null && !Scene.IsEditor)
 		{
 			var go = SceneUtility.GetPrefabScene(DefaultPrefab).Clone();
-			TryAttachGameObject(go);
+			TryAttachGameObject(go, true);
 		}
 	}
 
@@ -69,12 +84,31 @@ public class AttachPoint : Component, Component.ExecuteInEditor
 			DrawGuideModel();
 	}
 
-	float GetRotationDifference(Rotation a, Rotation b)
+	bool IsObjectCloseEnough(GameObject gameObject)
 	{
-		return a.Up.Dot(b.Up);
+		var pitchDiff = Math.Abs(
+			gameObject.Transform.Rotation.Pitch() - Transform.Rotation.Pitch()
+		);
+		var yawDiff = Math.Abs(gameObject.Transform.Rotation.Yaw() - Transform.Rotation.Yaw());
+		var rollDiff = Math.Abs(gameObject.Transform.Rotation.Roll() - Transform.Rotation.Roll());
+		var posDiff = (gameObject.Transform.Position - Transform.Position).Length;
+
+		if (!IgnorePitch && pitchDiff > AngThreshold)
+			return false;
+
+		if (!IgnoreYaw && yawDiff > AngThreshold)
+			return false;
+
+		if (!IgnoreRoll && rollDiff > AngThreshold)
+			return false;
+
+		if (posDiff > PosThreshold)
+			return false;
+
+		return true;
 	}
 
-	private void TryAttachGameObject(GameObject gameObject)
+	private void TryAttachGameObject(GameObject gameObject, bool force = false)
 	{
 		var attachable = gameObject.Components.Get<Attachable>();
 		if (!attachable.IsValid())
@@ -82,12 +116,10 @@ public class AttachPoint : Component, Component.ExecuteInEditor
 
 		if (attachable.AttachableType == TargetType)
 		{
-			var rotDiff = GetRotationDifference(Transform.Rotation, gameObject.Transform.Rotation);
-
-			if (rotDiff < 0.9f)
-				return;
-
-			attachable.AttachTo(this);
+			if (force || IsObjectCloseEnough(gameObject))
+			{
+				attachable.AttachTo(this);
+			}
 		}
 	}
 
